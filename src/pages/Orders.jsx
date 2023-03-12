219b3e2e8ac0992b464cd79c60abb0f5ae7b5ba0
Thien Nguyen <thieennt@gmail.com>
2023-03-12 22:01:28 +0700

    add data tables

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,7 +1,52 @@
+import {
+  ColumnDirective,
+  ColumnsDirective,
+  ContextMenu,
+  Edit,
+  ExcelExport,
+  Filter,
+  GridComponent,
+  Inject,
+  Page,
+  PdfExport,
+  Resize,
+  Sort,
+} from "@syncfusion/ej2-react-grids";
 import React from "react";
 
+import { Header } from "../components";
+import { ordersData, ordersGrid } from "../data/dummy";
+
 const Orders = () => {
-  return <div>Orders</div>;
+  return (
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Orders" />
+      <GridComponent
+        id="gridcomp"
+        dataSource={ordersData}
+        allowPaging
+        allowSorting
+      >
+        <ColumnsDirective>
+          {ordersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject
+          services={[
+            Resize,
+            Sort,
+            ContextMenu,
+            Filter,
+            Page,
+            ExcelExport,
+            Edit,
+            PdfExport,
+          ]}
+        />
+      </GridComponent>
+    </div>
+  );
 };
 
 export default Orders;
