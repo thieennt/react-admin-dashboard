5011e324d15d7cd34699da805c3d7eb26cfe295b
Thien Nguyen <thieennt@gmail.com>
2023-03-12 10:58:05 +0700

    done sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 import { Navbar, Footer, Sidebar, ThemeSetting } from "./components";
+
 import {
   Ecommerce,
   Orders,
