8671afc88c7f67da3450539a9362d503d12d1fb2
Thien Nguyen <thieennt@gmail.com>
2023-03-13 21:15:29 +0700

    update lineChart, areaChart

--- src/pages/Charts/Area.jsx
+++ src/pages/Charts/Area.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import {
+  ChartComponent,
+  SplineAreaSeries,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  DateTime,
+  Legend,
+  Tooltip,
+} from "@syncfusion/ej2-react-charts";
+
+import {
+  areaCustomSeries,
+  areaPrimaryYAxis,
+  areaPrimaryXAxis,
+} from "../../data/dummy";
+
+import { useStateContext } from "../../contexts/ContextProvider";
+import { Header } from "../../components";
 
 const Area = () => {
-  return <div>Area</div>;
+  const { currentMode } = useStateContext();
+  return (
+    <div className="p-10 m-4 mt-24 bg-white md:m-10 dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="Area" title="Inflation Rate In Percentage" />
+      <ChartComponent
+        id="area-chart"
+        height="420px"
+        primaryXAxis={areaPrimaryXAxis}
+        primaryYAxis={areaPrimaryYAxis}
+        chartArea={{ border: { width: 0 } }}
+        tooltip={{ enable: true }}
+        background={currentMode === "Dark" ? "#33373E" : "#FFF"}
+      >
+        <Inject services={[SplineAreaSeries, DateTime, Legend, Tooltip]} />
+        <SeriesCollectionDirective>
+          {areaCustomSeries.map((item, index) => (
+            <SeriesDirective key={index} {...item} />
+          ))}
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    </div>
+  );
 };
 
 export default Area;
--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Header, LineChart } from "../../components";
 
 const Line = () => {
-  return <div>Line</div>;
+  return (
+    <div className="p-10 m-4 mt-24 bg-white md:m-10 dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="Chart" title="Inflation Rate" />
+      <div className="w-full">
+        <LineChart />
+      </div>
+    </div>
+  );
 };
 
 export default Line;
